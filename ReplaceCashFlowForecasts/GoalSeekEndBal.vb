7b4b7b8e42c3ca3bd84c479038c2425fa30769a3
Jeremy Bharwani <jeremyclark.bharwani@calbaptist.edu>
2021-08-13 14:38:05 -0700

    Added GoalSeek Macro

---
+++ ReplaceCashFlowForecasts/GoalSeekEndBal.vb
@@ -0,0 +1,15 @@
+﻿Sub GoalSeekEndBal()
+'
+' GoalSeekEndBal Macro
+' Created by Jeremy Bharwani on 7/19/21
+' (questions- email jcb926@gmail.com)
+'
+' This macro uses What-If Analysis to goal seek an ending balance of 0. Your cursor must be on the bottom-most
+' cell of the end balance column to set that cell to 0.
+'
+
+Set EndingBal = ActiveCell
+    Set ChangeValue = Cells(EndingBal.row + 3, EndingBal.Column - 5)
+    
+    Range(EndingBal.Address).GoalSeek Goal:=0, ChangingCell:=Range(ChangeValue.Address)
+End Sub
